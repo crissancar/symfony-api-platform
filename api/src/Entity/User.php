1afc6b4ce9ad051d306382280e17b4702ddd598e
crissancar <cristiansanchezcarr@gmail.com>
2020-09-23 00:32:43 +0200

    - Login

--- api/src/Entity/User.php
+++ api/src/Entity/User.php
@@ -1,5 +1,6 @@
 <?php
 
+declare(strict_types=1);
 
 namespace App\Entity;
 
@@ -20,11 +21,6 @@ class User implements UserInterface
     private \DateTime $createdAt;
     private \DateTime $updatedAt;
 
-    /**
-     * User constructor.
-     * @param string $name
-     * @param string $email
-     */
     public function __construct(string $name, string $email)
     {
         $this->id = Uuid::v4()->toRfc4122();
@@ -39,148 +35,95 @@ class User implements UserInterface
         $this->markAsUpdated();
     }
 
-    /**
-     * @return string
-     */
     public function getId(): string
     {
         return $this->id;
     }
 
-    /**
-     * @return string
-     */
     public function getName(): string
     {
         return $this->name;
     }
 
-    /**
-     * @param string $name
-     */
     public function setName(string $name): void
     {
         $this->name = $name;
     }
 
-    /**
-     * @return string
-     */
     public function getEmail(): string
     {
         return $this->email;
     }
 
-    /**
-     * @param string $email
-     */
     public function setEmail(string $email): void
     {
-        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        if (!\filter_var($email, \FILTER_VALIDATE_EMAIL)) {
             throw new \LogicException('Invalid email');
         }
+
         $this->email = $email;
     }
 
-    /**
-     * @return string|null
-     */
     public function getPassword(): ?string
     {
         return $this->password;
     }
 
-    /**
-     * @param string|null $password
-     */
     public function setPassword(?string $password): void
     {
         $this->password = $password;
     }
 
-    /**
-     * @return string|null
-     */
     public function getAvatar(): ?string
     {
         return $this->avatar;
     }
 
-    /**
-     * @param string|null $avatar
-     */
     public function setAvatar(?string $avatar): void
     {
         $this->avatar = $avatar;
     }
 
-    /**
-     * @return string|null
-     */
     public function getToken(): ?string
     {
         return $this->token;
     }
 
-    /**
-     * @param string|null $token
-     */
     public function setToken(?string $token): void
     {
         $this->token = $token;
     }
 
-    /**
-     * @return string|null
-     */
     public function getResetPasswordToken(): ?string
     {
         return $this->resetPasswordToken;
     }
 
-    /**
-     * @param string|null $resetPasswordToken
-     */
     public function setResetPasswordToken(?string $resetPasswordToken): void
     {
         $this->resetPasswordToken = $resetPasswordToken;
     }
 
-    /**
-     * @return bool
-     */
     public function isActive(): bool
     {
         return $this->active;
     }
 
-    /**
-     * @param bool $active
-     */
     public function setActive(bool $active): void
     {
         $this->active = $active;
     }
 
-    /**
-     * @return \DateTime
-     */
     public function getCreatedAt(): \DateTime
     {
         return $this->createdAt;
     }
 
-    /**
-     * @return \DateTime
-     */
     public function getUpdatedAt(): \DateTime
     {
         return $this->updatedAt;
     }
 
-    /**
-     * @param \DateTime $updatedAt
-     */
     public function markAsUpdated(): void
     {
         $this->updatedAt = new \DateTime();
@@ -203,4 +146,5 @@ class User implements UserInterface
     public function eraseCredentials(): void
     {
     }
+
 }
